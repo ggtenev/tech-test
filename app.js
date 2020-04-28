280f8caa5c8c0ea8ba06ed63eef7bc1c421ba849
ggtenev <georgiy.tenev@gmail.com>
2020-04-29 00:52:03 +0100

    final

--- app.js
+++ app.js
@@ -9,15 +9,18 @@ mobileAbout.addEventListener('click', e => {
   console.log(44)
   if(aboutContent.style.display === 'none'){
     aboutContent.style.display = 'flex';
+    aboutContent.style.maxHeight = `${300}px`
+
   } else{
     aboutContent.style.display = 'none';
+    aboutContent.style.maxHeight = null
   }
 })
 
 
 //Toggle Mobile navigation
 
-
+ 
 const bars = document.querySelector('.fa-bars')
 const menuOverlay = document.querySelector('.menu-overlay')
 const closeOverlay = document.querySelector('.fa-times')
@@ -25,11 +28,13 @@ const closeOverlay = document.querySelector('.fa-times')
 bars.addEventListener('click', e => {
   e.preventDefault()
   menuOverlay.style.display = 'block';
+  menuOverlay.style.opacity = '1';
 })
 
 closeOverlay.addEventListener('click', e => {
   e.preventDefault()
   menuOverlay.style.display = 'none';
+  menuOverlay.style.opacity = '0';
 })
 
 
